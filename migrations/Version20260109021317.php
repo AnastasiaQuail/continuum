951587d66413c573358b4ab24403391e8f8f08da
AnastasiaQuail <akikagaro@gmail.com>
2026-01-09 05:14:14 +0300

    change calendar_days: remove old table

---
+++ migrations/Version20260109021317.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260109021317 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE calendar_days');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<'SQL'
+            CREATE TABLE calendar_days (
+              id UUID NOT NULL,
+              date DATE NOT NULL,
+              type VARCHAR(255) NOT NULL,
+              title VARCHAR(255) NOT NULL,
+              "time" TIME(0) WITHOUT TIME ZONE DEFAULT NULL,
+              PRIMARY KEY (id)
+            )
+        SQL);
+    }
+}
