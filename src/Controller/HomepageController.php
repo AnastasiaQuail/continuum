ebb7e4344cb687881f46c9191af0601c12513446
AnastasiaQuail <akikagaro@gmail.com>
2026-01-28 18:19:08 +0300

    add homepage dashboard controls

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,6 +12,7 @@ use Continuum\Service\Workout\WorkoutService;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
@@ -25,23 +26,28 @@ final class HomepageController extends AbstractController
     ) {}
 
     #[Route(path: '/', name: 'app_homepage')]
-    public function __invoke(#[CurrentUser] User $user): Response
-    {
+    public function __invoke(
+        #[CurrentUser] User $user,
+        #[MapQueryParameter('measurement', options: ['min_range' => 1])] int $measurementMonths = 2,
+        #[MapQueryParameter('workout', options: ['min_range' => 1])] int $workoutMonths = 2,
+    ): Response {
         $date = new DateTimeImmutable('now', $user->getTimezone())->setTime(0, 0);
-        $showDays = 60;
 
-        $prevDate = $date->modify(sprintf('-%d days', $showDays));
+        $measurementDays = 30 * $measurementMonths;
+        $prevDate = $date->modify(sprintf('-%d days', $measurementDays));
         $measurements = $this->measurementService->getByRange($user, $prevDate, $date);
         $chartMeasurements = $this->chartMeasurementService->getChartMeasurements($user, $prevDate, $measurements);
 
-        $prevDate = $date->modify(sprintf('-%d days', round($showDays / 7) * 7 + (int) $date->format('N')));
+        $workoutDays = round((30 * $workoutMonths) / 7) * 7 + (int) $date->format('N');
+        $prevDate = $date->modify(sprintf('-%d days', $workoutDays));
         $workouts = $this->workoutService->getByRange($user, $prevDate, $date);
 
         return $this->render('default/homepage.html.twig', [
             'calendarProgress' => $this->calendarProgressService->getCurrentProgress(),
-            'showDays' => $showDays,
             'chartMeasurements' => $chartMeasurements,
+            'measurementDays' => $measurementDays,
             'workouts' => $workouts,
+            'workoutDays' => $workoutDays,
         ]);
     }
 }
