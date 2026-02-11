697336d5ec6ff4baabde1f9785ad4d461e34b585
AnastasiaQuail <akikagaro@gmail.com>
2026-02-11 03:58:36 +0300

    add deploy workflow

--- deploy.php
+++ deploy.php
@@ -6,39 +6,37 @@ namespace Deployer;
 
 require 'recipe/symfony.php';
 
-set('repository', 'https://github.com/AnastasiaQuail/continuum.git');
-
 host('production')
     ->setHostname(getenv('APP_HOSTNAME'))
     ->setRemoteUser(getenv('APP_REMOTE_USER'))
     ->setDeployPath('~/domains/{{hostname}}')
     ->set('http_user', '{{remote_user}}');
 
+set('repository', 'https://github.com/AnastasiaQuail/continuum.git');
 set('keep_releases', 3);
-set('clear_paths', [
-    '.github',
-    'assets',
-    'frankenphp',
-    'tests',
-    '.dockerignore',
-    '.editorconfig',
-    '.env',
-    '.env.dev',
-    '.env.test',
-    '.gitattributes',
-    '.gitignore',
-    'compose.override.yaml',
-    'compose.prod.yaml',
-    'compose.yaml',
-    'deploy.php',
-    'Dockerfile',
-    'importmap.php',
-    'phpunit.dist.xml',
-    'Readme.md',
+set('deploy_paths', [
+    'bin',
+    'config',
+    'data',
+    'migrations',
+    'public',
+    'src',
+    'templates',
+    'var',
+    'vendor',
+    '.env.local',
+    '.env.local.php',
+    'composer.json',
 ]);
 
 task('deploy:assets:compile', static function (): void {
-    run('cd {{release_or_current_path}} && {{bin/console}} asset-map:compile {{console_options}}');
+    run('cd {{release_path}} && {{bin/console}} asset-map:compile {{console_options}}');
+});
+task('deploy:paths:clear', function () {
+    $paths = explode("\n", run("cd {{release_path}} && find . -maxdepth 1 -mindepth 1 -printf '%f\n'"));
+
+    set('clear_paths', array_values(array_diff($paths, get('deploy_paths'))));
+    invoke('deploy:clear_paths');
 });
 task('deploy:symlink:public', static function (): void {
     run('{{bin/symlink}} {{current_path}}/public {{deploy_path}}/public_html');
@@ -46,7 +44,7 @@ task('deploy:symlink:public', static function (): void {
 
 after('deploy:vendors', 'deploy:dump-env');
 after('deploy:cache:clear', 'deploy:assets:compile');
-after('deploy:cache:clear', 'deploy:clear_paths');
+after('deploy:cache:clear', 'deploy:paths:clear');
 before('deploy:publish', 'database:migrate');
 after('deploy:symlink', 'deploy:symlink:public');
 
