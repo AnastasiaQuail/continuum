11a79402e68ec62516761a6382fe9df0b56c9762
AnastasiaQuail <akikagaro@gmail.com>
2026-02-13 17:04:17 +0300

    move PHPStan to main composer.json

--- tests/console-application.php
+++ tests/console-application.php
@@ -6,9 +6,9 @@ use Continuum\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-new Dotenv()->bootEnv(__DIR__ . '/../.env');
+new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 
